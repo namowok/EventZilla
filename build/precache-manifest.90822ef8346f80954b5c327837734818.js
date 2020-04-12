e732cbf5023f0024acd39432580e2cb0d2fc0ad9
nayanjain540 <nayanjain540@gmail.com>
2020-04-12 16:44:43 +0530

    Added serve

---
+++ build/precache-manifest.90822ef8346f80954b5c327837734818.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568ca2a1c258fce4e82b2be0d7848a66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f29422d98459d87d4921",
+    "url": "/static/css/2.afb8e144.chunk.css"
+  },
+  {
+    "revision": "67fc9b36846fed2ee060",
+    "url": "/static/css/main.e1b2b0a0.chunk.css"
+  },
+  {
+    "revision": "f29422d98459d87d4921",
+    "url": "/static/js/2.37f6f73b.chunk.js"
+  },
+  {
+    "revision": "72a51b6d04ceec5c2fc9a1a89a1c6d58",
+    "url": "/static/js/2.37f6f73b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67fc9b36846fed2ee060",
+    "url": "/static/js/main.8dc759d1.chunk.js"
+  },
+  {
+    "revision": "6f2a91aeffb53d611b25",
+    "url": "/static/js/runtime-main.6388c265.js"
+  },
+  {
+    "revision": "dc927289df508bbe45258d30f16170b6",
+    "url": "/static/media/image1.dc927289.png"
+  },
+  {
+    "revision": "f5c4d79a7300a5da5ecb454cd3922de2",
+    "url": "/static/media/image2.f5c4d79a.png"
+  },
+  {
+    "revision": "491602db8980ecad0d540987dfde17a0",
+    "url": "/static/media/image3.491602db.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
